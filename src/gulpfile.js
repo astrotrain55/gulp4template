44f93b9a938ce4b59e7fa074195919c5d2ab4e42
zvo <astrotrain55@gmail.com>
2018-09-20 13:37:18 +0500

    add webpack

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -9,13 +9,6 @@ global.$ = {
   gulp: require('gulp'),
   load: require('gulp-load-plugins')(),
 
-  gcmq: require('gulp-group-css-media-queries'),
-  include: require('gulp-file-include'),
-  spritesmith: require("gulp.spritesmith"),
-  svg: require('gulp-svg-sprites'),
-
-  del: require('del'),
-  nib: require('nib'),
   sync: require('browser-sync').create()
 };
 
@@ -28,7 +21,6 @@ $.gulp.task('clear_cache', () => {
 });
 
 $.gulp.task('default', $.gulp.series(
-  $.gulp.parallel('vendorCSS', 'vendorJS'),
-  $.gulp.parallel('pug', 'stylus', 'js'),
+  $.gulp.parallel('vendor', 'stylus', 'pug', 'js'),
   $.gulp.parallel('serve', 'watch', 'ie')
 ));
